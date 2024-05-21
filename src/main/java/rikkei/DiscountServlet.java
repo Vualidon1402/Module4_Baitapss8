5e18381e07d19878c347394a672cb0bbb1176e03
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-05-20 19:08:35 -0700

    bai tap ss9

---
+++ src/main/java/rikkei/DiscountServlet.java
@@ -0,0 +1,36 @@
+package rikkei;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DiscountServlet", value = "/display-discount")
+public class DiscountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String description = request.getParameter("Product_Description");
+        String price = request.getParameter("List_Price");
+        String discount = request.getParameter("Discount_Percen");
+        double price1 = Double.parseDouble(price);
+        double discount1 = Double.parseDouble(discount);
+
+        double discountAmount = price1 * discount1 / 100;
+        double discountPrice = price1 - discountAmount;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>Product Description: " + description + "</h1></body></html>");
+        out.println("<html><body><h1>Discount Amount: " + discountAmount + "</h1></body></html>");
+        out.println("<html><body><h1>Discount Price: " + discountPrice + "</h1></body></html>");
+
+
+    }
+}
